6dbbb7f9d9d6d6930b29d11f4e64c9cf10a6e2ce
Francois Szczepkowski <francois.szcz@hotmail.fr>
2021-10-21 10:15:01 +0200

    first commit
    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <MyParagraphe text="Bonjour" color="green"/>
+    <MyParagraphe :text="message"/>
+    <h1>Counter: {{counter}}</h1>
+    <p>{{message}}</p>
+    <button @click.prevent="increment">Incrémenter le compteur</button>
+    <h2>Counter x2: {{doubledCounter}}</h2>
+  </div>
+  <button @click="toggleDiv">Afficher la div</button>
+  <div class="div" v-if="isDivDisplayed">Une div</div>
+</template>
+
+<script>
+import MyParagraphe from './components/MyParagraphe.vue'
+import { computed, ref } from 'vue'
+export default {
+  components: {
+    MyParagraphe
+  },
+  setup () {
+    const message = ref('Un beau mesage oui <3')
+
+    const counter = ref(0)
+
+    const increment = () => {
+      counter.value++
+    }
+
+    const doubledCounter = computed(() => {
+      return counter.value * 2
+    })
+
+    const isDivDisplayed = ref(true)
+
+    const toggleDiv = () => {
+      isDivDisplayed.value = !isDivDisplayed.value
+    }
+
+    return {
+      message,
+      counter,
+      increment,
+      doubledCounter,
+      isDivDisplayed,
+      toggleDiv
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.div{
+  background-color: red;
+  min-height: 300px;
+}
+</style>
---
+++ src/components/MyParagraphe.vue
@@ -0,0 +1,47 @@
+<template>
+    <p>{{ text }}</p>
+    <p class="my-paragraphe" :class="classes">
+      {{ text }}
+    </p>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: 'black'
+    }
+  },
+  setup (props) {
+    const classes = computed(() => {
+      return `my-paragraphe--${props.color}`
+    })
+
+    return {
+      classes
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    .my-paragraphe {
+      color: black;
+
+      &--red{
+        color: red;
+      }
+      &--blue{
+        color: blue;
+      }
+      &--green{
+        color: green;
+      }
+    }
+</style>
